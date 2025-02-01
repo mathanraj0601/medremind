870ce31eba1fe8a334d3fc0647c664263e0569b1
Mathanraj <madhanmani0601@gmail.com>
2025-02-01 17:44:30 +0530

    Updated folder structure

--- medremind.mobile/app/index.tsx
+++ medremind.mobile/app/index.tsx
@@ -1,12 +1,64 @@
-import { Text, View } from "react-native";
-import { useEffect } from "react";
-import { useRouter } from "expo-router";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useEffect, useRef } from "react";
+import { StyleSheet, View, Text, Animated } from "react-native";
+
+export default function SplashScreen() {
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
+  const colors = useThemeColor();
+
+  useEffect(()=>{
+    Animated.parallel([
+      Animated.timing(fadeAnim,{
+        toValue : 1,
+        duration : 1000,
+        useNativeDriver : true
+      }),
+      Animated.spring(scaleAnim,{
+        toValue : 1,
+        useNativeDriver : true,
+        tension : 10,
+        friction : 2
+      }),
+    ]).start()
+
+    const timer = setTimeout(()=>{ router.replace('/auth/auth')},1000)
+    return () => clearTimeout(timer)
+  },[router])
+  return <View style={[styles.splashScreenContainer,  { backgroundColor : colors.primary } ]}>
+    <Animated.View style={[styles.iconContainer,
+      {
+        opacity : fadeAnim,
+        transform : [{scale : scaleAnim}]
+      }
+    ]}>
+      <Ionicons name="medical" size={150} color={colors.white} />
+      <Text style={[styles.appName , {color : colors.white}]}> { text.title.toUpperCase() }</Text>
+    </Animated.View>
+  </View>
+}
+
+const styles = StyleSheet.create({
+  splashScreenContainer: {
+    flex:1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer : {
+    alignItems:'center'
+   },
+   appName : {
+    fontSize : 24,
+    fontWeight : "semibold",
+    letterSpacing:1
+   }
+});
 
 
-export default function App() {
-    const router= useRouter();
-    useEffect(()=>{ router.replace('/auth/auth')},[router])
-    return <View>
-        <Text>one</Text>
-    </View>;
+const text = {
+  title : 'Medremind'
 }
